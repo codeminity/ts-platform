ce429958a5476873e0d037e76c7abdda1da7354a
masoudfooladi-me <masoudfooladi.me@gmail.com>
2026-07-11 14:34:30 +0330

    feat(axios): add error event callback emitter

---
+++ packages/request/axios/src/emit/error-event.ts
@@ -0,0 +1,12 @@
+import type { CallbackConfig } from '../interfaces/callback-config'
+import type { ErrorEvent } from '../types/error-event'
+import type { AxiosError } from 'axios'
+
+export async function emitterCallback(
+  event: ErrorEvent,
+  error: AxiosError,
+  config: CallbackConfig
+): Promise<void> {
+  await config.onEvent?.(event, error)
+  await config.onError?.(error)
+}
