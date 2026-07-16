b4f3c71da93d8bc9a2e5ed8a99dc76c818f77036
masoudfooladi-me <masoudfooladi.me@gmail.com>
2026-07-16 13:33:52 +0330

    fix(axios): expose codeminity types through public entrypoint

--- packages/request/axios/src/index.ts
+++ packages/request/axios/src/index.ts
@@ -3,6 +3,19 @@ import axios from 'axios'
 import { create } from './factories/create'
 import { getAxiosInstance } from './utils/get-axios-instance'
 
+import type { Config } from './interfaces/config'
+import type { RequestConfig } from './interfaces/request-config'
+
+declare module 'axios' {
+  interface CreateAxiosDefaults {
+    codeminity?: Config
+  }
+
+  interface AxiosRequestConfig {
+    codeminity?: RequestConfig
+  }
+}
+
 export default Object.assign(getAxiosInstance, axios, { create })
 
 export * from 'axios'
