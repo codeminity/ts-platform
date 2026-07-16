504f7e9488c56298d90374d884030c4008d420b5
masoudfooladi-me <masoudfooladi.me@gmail.com>
2026-07-16 14:59:51 +0330

    refactor: reuse retry config types in request config

--- packages/request/axios/src/interfaces/request-config.ts
+++ packages/request/axios/src/interfaces/request-config.ts
@@ -1,10 +1,8 @@
+import type { RetryConfig } from './retry-config'
 import type { InternalAxiosRequestConfig } from 'axios'
 
-export interface RequestConfig {
+export interface RequestConfig extends Pick<RetryConfig, 'retries' | 'retryDelay'> {
   skipAuth?: boolean
-
-  retries?: number
-  retryDelay?: number
 }
 
 export interface InternalRequestConfig extends InternalAxiosRequestConfig {
